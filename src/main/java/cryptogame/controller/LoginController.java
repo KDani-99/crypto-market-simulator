2e7f9547360a45d1e1aeb1f000f2d00b539b5501
KDani-99 <porsekin@gmail.com>
2021-03-31 13:09:17 +0200

    Refactor LoginController

--- src/main/java/cryptogame/controller/LoginController.java
+++ src/main/java/cryptogame/controller/LoginController.java
@@ -3,6 +3,7 @@ package cryptogame.controller;
 import cryptogame.model.session.SessionManager;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
@@ -10,14 +11,17 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
 
+import java.util.HashMap;
 
-public class LoginController {
 
-    private final Scene scene;
+public class LoginController extends BaseController {
+
+    public static final String SCENE_NAME = "login";
+
     private final SessionManager sessionManager;
 
     private TextField usernameInput;
-    private TextField passwordInput;
+    private PasswordField passwordInput;
 
     private Button loginButton;
     private Button registerButton;
@@ -25,22 +29,26 @@ public class LoginController {
     private Pane errorPane;
     private Label errorLabel;
 
-    public LoginController(Stage stage, SessionManager sessionManager) {
-        this.scene = stage.getScene();
+    public LoginController(Stage primaryStage,HashMap<String, Scene> scenes, SessionManager sessionManager) {
+
+        super(primaryStage,scenes,SCENE_NAME);
+
         this.sessionManager = sessionManager;
         this.getSceneNodes();
     }
 
-    private void getSceneNodes() {
+    @Override
+    protected void getSceneNodes() {
         this.usernameInput = (TextField)this.scene.lookup("#input_username");
         this.setupUsernameInput();
 
-        this.passwordInput = (TextField)this.scene.lookup("#input_password");
+        this.passwordInput = (PasswordField) this.scene.lookup("#input_password");
 
         this.loginButton = (Button)this.scene.lookup("#button_login");
         this.setupLoginButton();
 
         this.registerButton = (Button)this.scene.lookup("#button_register");
+        this.setupRegisterButton();
 
         this.errorPane = (Pane)this.scene.lookup("#pane_error");
         errorPane.setVisible(false);
@@ -58,6 +66,8 @@ public class LoginController {
             public void handle(MouseEvent event) {
                 try {
 
+                    errorPane.setVisible(false);
+
                     var username = usernameInput.getText();
                     var password = passwordInput.getText();
 
@@ -66,6 +76,19 @@ public class LoginController {
                 } catch (Exception ex) {
                     errorLabel.setText(ex.getMessage());
                     errorPane.setVisible(true);
+                }
+            }
+        });
+    }
+    private void setupRegisterButton() {
+        this.registerButton.setOnMouseClicked(new EventHandler<>(){
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    getPrimaryStage().setScene(getScene(RegistrationController.SCENE_NAME));
+                    getPrimaryStage().setResizable(false);
+                } catch (Exception ex) {
+                    System.out.println(ex.toString());
                 }
             }
         });
