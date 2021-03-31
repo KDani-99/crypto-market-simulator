57ebe630b4c01b29b6cb11c6a7386431ce9ca4ad
KDani-99 <porsekin@gmail.com>
2021-03-31 19:48:37 +0200

    Refactor Main

--- src/main/java/cryptogame/Main.java
+++ src/main/java/cryptogame/Main.java
@@ -23,68 +23,30 @@ import java.util.Properties;
 public class Main extends Application {
 
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("crypto_trading_game");
+
     UserDao usersDao = null;
     SessionDao sessionDao = null;
     SessionManager sessionManager = null;
     EntityManager entityManager = null;
 
-  //  LoginController loginController;
     Controller controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // test
-
-        // TEST
-        //databaseHandler = new H2Service();
-       // databaseHandler.connect();
         entityManager = entityManagerFactory.createEntityManager();
         usersDao = new UserDao(entityManager);
         sessionDao = new SessionDao(entityManager);
 
         sessionManager = new SessionManager(usersDao,sessionDao);
-        try {
-;
-            sessionManager.register("Daniel","kdani98@mailbox.unideb.hu","ThiSisM@246","ThiSisM@246");
-            System.out.println("Registered user");
-        } catch (Exception ex) {
-            System.out.println("ERROR (register)! "+ex.getMessage());
-            try {
-                sessionManager.login("Daniel","ThiSisM@246");
-                System.out.println("Logged in");
-            } catch (Exception ex2) {
-                System.out.println("ERROR (login) ! "+ex2.getMessage());
-            }
-        }
 
-       // Parent root = FXMLLoader.load(Main.class.getResource("/views/MainView.fxml"));
-       // Parent root = FXMLLoader.load(Main.class.getResource("/views/login/LoginView.fxml"));
 
         Properties properties = new Properties();
         properties.load(Main.class.getResourceAsStream("/application.properties"));
 
-        //primaryStage.setTitle("Crypto Trading Game");
 
-        //primaryStage.setScene(new Scene(root, 400, 500));
-        //primaryStage.setResizable(false);
-
-     //   ((Label)primaryStage.getScene().lookup("#label_version")).setText("v"+properties.get("version").toString());
-       /* primaryStage.setMinWidth(300);
-        primaryStage.setMaxWidth(300);
-        primaryStage.setMaxHeight(450);
-        primaryStage.setMinHeight(450);*/
-
-       // primaryStage.show();
-
-        //HashMap<String ,Scene> scenes = new HashMap<>();
-        //scenes.put(root.getId)
-
-
-        controller = new Controller(primaryStage,sessionManager);
+        controller = new Controller(properties,primaryStage,sessionManager);
         controller.setupScenes();
-        controller.registerControllers();
-       // loginController = new LoginController(primaryStage,sessionManager);
 
     }
 
