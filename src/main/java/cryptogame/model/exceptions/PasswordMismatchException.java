c45823d5373b82c9980afd685f9e0c3a67e0df2d
KDani-99 <porsekin@gmail.com>
2021-03-31 19:46:23 +0200

    Remove password verification field (and now it's unused)

--- src/main/java/cryptogame/model/exceptions/PasswordMismatchException.java
+++
@@ -1,7 +0,0 @@
-package cryptogame.model.exceptions;
-
-public class PasswordMismatchException extends Exception {
-    public PasswordMismatchException() {
-        super("Passwords must be the same");
-    }
-}
