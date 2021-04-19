6929415f9d0bb2599c5aedb95e780ec6acf914c2
KDani-99 <porsekin@gmail.com>
2021-04-19 16:08:11 +0200

    Add AreaChart field

--- src/main/java/cryptogame/controller/main/HomeController.java
+++ src/main/java/cryptogame/controller/main/HomeController.java
@@ -1,4 +1,10 @@
 package cryptogame.controller.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.AreaChart;
+
 public class HomeController {
+
+    @FXML public AreaChart<Double,Double>  areaChart;
+
 }
