0f7e94ba5f623a5e7572b48de72ae3785bcdf7c2
KDani-99 <porsekin@gmail.com>
2021-04-23 23:18:29 +0200

    Rename Market and Controller

--- src/main/java/cryptogame/controller/Controller.java
+++ src/main/java/cryptogame/controller/Controller.java
@@ -1,13 +1,17 @@
 package cryptogame.controller;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-public interface IController {
+import java.net.URL;
+
+public interface Controller extends ComponentController {
     void initScene();
     void showError(String message,String alertMessage);
     void hideError();
     void createScene(Parent view);
     Scene getScene();
     boolean isResizable();
+    URL getResourceURL();
 }
