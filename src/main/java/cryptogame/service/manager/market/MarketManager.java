2280cb03c7ca37f6926821ba6a03961fe71af89b
KDani-99 <porsekin@gmail.com>
2021-04-24 09:37:17 +0200

    Add MarketManager interface methods

--- src/main/java/cryptogame/service/manager/market/MarketManager.java
+++ src/main/java/cryptogame/service/manager/market/MarketManager.java
@@ -1,4 +1,11 @@
 package cryptogame.service.manager.market;
 
+import cryptogame.containers.CryptoCurrency;
+
+import java.util.Collection;
+import java.util.Set;
+
 public interface MarketManager {
+    Collection<CryptoCurrency> getCurrencies();
+    void refreshAssets() throws Exception;
 }
