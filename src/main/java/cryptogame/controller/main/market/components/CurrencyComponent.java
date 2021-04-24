03cfaf9c2192dc103535e701092ca8c427cb167d
KDani-99 <porsekin@gmail.com>
2021-04-24 09:37:56 +0200

    Handle container data

--- src/main/java/cryptogame/controller/main/market/components/CurrencyComponent.java
+++ src/main/java/cryptogame/controller/main/market/components/CurrencyComponent.java
@@ -4,6 +4,7 @@ import cryptogame.common.Initializable;
 import cryptogame.containers.CryptoCurrency;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.apache.commons.codec.digest.Crypt;
 
 public class CurrencyComponent implements Initializable {
 
@@ -19,29 +20,72 @@ public class CurrencyComponent implements Initializable {
     public void setCurrency(CryptoCurrency currency) {
         this.currency = currency;
     }
+    public CryptoCurrency getCurrency() {
+        return this.currency;
+    }
 
     @Override
     public void initialize() {
         if(currency == null) return;
 
-        this.handleChange(); // @test
-        this.bindCurrencyData();
+        this.bindData();
     }
 
-    private void bindCurrencyData() {
+    private void bindData() {
 
+        this.formatRank();
+
+        this.formatName();
+
+        this.formatPrice();
+
+        this.formatMarketCap();
+
+        this.formatSupply();
+
+        this.formatChange();
+    }
+
+    private String formatBigNumber(double n) {
+        if(n >= 1_000_000_000) {
+            return String.format("$%.2fb",n / 1_000_000_000d);
+        } else if(n >= 1_000_000) {
+            return String.format("$%.2fm",n / 1_000_000d);
+        } else {
+            return String.format("$%.2f",n);
+        }
+    }
+
+    private void formatRank() {
         this.rankLabel.setText("#"+currency.getRank());
-
-        this.nameLabel.setText(currency.getName() + "(" + "$"+ currency.getId() +")");
-
-        this.priceLabel.setText("$"+currency.getPriceUsd());
-
-        this.marketCapLabel.setText("$"+currency.getMarketCapUsd()); // !! -> add 'b'
-
-        this.supplyLabel.setText(Double.toString(currency.getSupply())); // !! -> add 'b'
     }
 
-    private void handleChange() {
+    private void formatName() {
+        this.nameLabel.setText(currency.getName());
+    }
+
+    private void formatSupply() {
+        var supply = currency.getSupply();
+        var text = formatBigNumber(supply);
+        this.supplyLabel.setText(text);
+    }
+
+    private void formatPrice() {
+        var price = currency.getPriceUsd();
+
+        this.priceLabel.setText(String.format("$%.2f",price));
+    }
+
+    private void formatMarketCap() {
+        var marketCap = currency.getMarketCapUsd();
+        var text = formatBigNumber(marketCap);
+        this.marketCapLabel.setText(text);
+    }
+
+    private void formatChange() {
+
+        this.changePercentLabel.setText(String.format("%.2f",currency.getChangePercent24Hr()) + "%");
+
         if(currency.getChangePercent24Hr() >= 0.0d) {
             changePercentLabel.getStyleClass().add("posChangePercent");
         } else {
