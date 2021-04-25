679ce2bd31377a02179174ada603fcfec9c6f023
KDani-99 <porsekin@gmail.com>
2021-04-25 15:51:34 +0200

    Add UserDao interface

--- src/main/java/cryptogame/dao/user/UserDao.java
+++ src/main/java/cryptogame/dao/user/UserDao.java
@@ -1,55 +1,11 @@
-package cryptogame.dao;
+package cryptogame.dao.user;
 
+import cryptogame.containers.CurrencyContainer;
+import cryptogame.dao.Dao;
+import cryptogame.jpa.entities.CryptoCurrency;
 import cryptogame.jpa.entities.User;
-import cryptogame.jpa.entities.UserSettings;
 
-import javax.persistence.EntityManager;
-import java.util.Optional;
-import java.util.UUID;
-
-public final class UserDao extends Dao<User> {
-
-    public UserDao(EntityManager entityManager) {
-        super(entityManager);
-    }
-
-   /* private String generateUID() {
-        return UUID.randomUUID().toString();
-    }*/
-
-    @Override
-    public Optional<User> getEntity(String id) {
-        var user = this.entityManager.find(User.class,id);
-        return Optional.of(user);
-    }
-    @Override
-    public Optional<User> getEntityBy(String field, Object value) {
-
-        var query = this.entityManager.createQuery("SELECT user FROM users user WHERE "+field+" = :value",User.class);
-        query.setParameter("value",value);
-
-        User user = null;
-
-        try {
-            user = query.getSingleResult();
-        } catch (Exception ex) {
-            user = null;
-        }
-
-        return Optional.ofNullable(user);
-    }
-
-    @Override
-    public void persistEntity(User entity) throws Exception {
-        entity.setId(this.generateUID());
-
-        var userSettings = new UserSettings();
-        userSettings.setUserId(entity.getId());
-        userSettings.setUser(entity);
-
-        //entity.setSettings(userSettings);
-
-        this.executeTransaction(entityManager -> entityManager.persist(entity));
-        this.executeTransaction(entityManager -> entityManager.persist(userSettings));
-    }
+public interface UserDao extends Dao<User>
+{
+    void purchaseCurrency(User user, double amount , CurrencyContainer currency);
 }
