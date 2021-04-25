535876391dbe60233a556c523d6ff0f7cfa10220
KDani-99 <porsekin@gmail.com>
2021-04-25 15:51:09 +0200

    Rename Dao -> DaoBase

---
+++ src/main/java/cryptogame/dao/DaoBase.java
@@ -0,0 +1,42 @@
+package cryptogame.dao;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public abstract class DaoBase<T> implements Dao<T> {
+
+    protected final EntityManager entityManager;
+
+    protected DaoBase(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public abstract <TId> Optional<T> getEntity(TId id);
+
+    @Override
+    public abstract Optional<T> getEntityBy(String field, Object value);
+
+    @Override
+    public void updateEntity(T entity) throws Exception {
+        this.executeTransaction(entityManager -> entityManager.merge(entity));
+    }
+
+    @Override
+    public void persistEntity(T entity) throws Exception {
+        this.executeTransaction(entityManager -> entityManager.persist(entity));
+    }
+
+    @Override
+    public void deleteEntity(T entity) throws Exception {
+        this.executeTransaction(entityManager -> entityManager.remove(entity));
+    }
+    // @Transactional
+    protected void executeTransaction(Consumer<EntityManager> consumer) {
+        var transaction = this.entityManager.getTransaction();
+        transaction.begin();
+        consumer.accept(this.entityManager);
+        transaction.commit();
+    }
+}
