59998fec955cdbc0cbb3a4bfb0d7a28f56c262f8
KDani-99 <porsekin@gmail.com>
2021-04-26 13:03:45 +0200

    Implement new controller factory methods

--- src/main/java/cryptogame/services/manager/scene/DefaultSceneManager.java
+++ src/main/java/cryptogame/services/manager/scene/DefaultSceneManager.java
@@ -2,16 +2,23 @@ package cryptogame.services.manager.scene;
 
 import cryptogame.Main;
 import cryptogame.controllers.*;
+import cryptogame.controllers.dialog.PurchaseDialogController;
 import cryptogame.controllers.main.market.MarketController;
+import cryptogame.controllers.main.market.components.CurrencyComponent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.naming.ldap.Control;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
 
 @Component("sceneManager")
 public class DefaultSceneManager implements SceneManager {
@@ -20,6 +27,9 @@ public class DefaultSceneManager implements SceneManager {
 
     private final HashMap<Class<?>, URL> controllerCollection;
     private final HashMap<Class<?>, URL> mainControllerComponents;
+    private final HashMap<Class<?>, URL> dialogControllerCollection;
+
+    private final HashMap<Class<?>,Stage> activeStages;
 
     //private final ServiceHandler services;
     private final Stage primaryStage;
@@ -30,9 +40,13 @@ public class DefaultSceneManager implements SceneManager {
         this.primaryStage = primaryStage;
         this.controllerCollection = new HashMap<>();
         this.mainControllerComponents = new HashMap<>();
+        this.dialogControllerCollection = new HashMap<>();
+
+        activeStages = new HashMap<>();
 
         this.addControllers();
         this.addMainControllerComponents();
+        this.addDialogControllers();
     }
 
     private void loadDefaultScene() throws Exception{
@@ -49,7 +63,11 @@ public class DefaultSceneManager implements SceneManager {
         this.controllerCollection.put(RegistrationController.class,getResourceURL("/views/registration/RegistrationView.fxml"));
     }
     private void addMainControllerComponents() {
+        this.mainControllerComponents.put(CurrencyComponent.class,getResourceURL("/views/app/components/market/components/CurrencyComponent.fxml"));
         this.mainControllerComponents.put(MarketController.class,getResourceURL("/views/app/components/market/MarketView.fxml"));
+    }
+    private void addDialogControllers() {
+        this.dialogControllerCollection.put(PurchaseDialogController.class,getResourceURL("/views/dialog/purchase/PurchaseDialogView.fxml"));
     }
 
     private <T extends WindowController> WindowController loadController(Class<T> controller) throws Exception {
@@ -68,7 +86,7 @@ public class DefaultSceneManager implements SceneManager {
     }
 
     private <T extends Controller> Controller loadControllerComponent(Class<T> controller) throws Exception {
-        var controllerInstance = context.getBean(controller);
+        var controllerInstance = context.getBean(controller); // create new bean
 
         var resourceUrl = mainControllerComponents.get(controller);
 
@@ -80,6 +98,29 @@ public class DefaultSceneManager implements SceneManager {
         return controllerInstance;
     }
 
+    private <T extends Controller> Controller createDialog(Class<T> dialog,String title,int width, int height, boolean resizable) throws Exception {
+        var controllerInstance = (Controller) context.getBean(dialog);
+
+        var resourceUrl = dialogControllerCollection.get(dialog);
+
+        var loader = new FXMLLoader(resourceUrl);
+        loader.setController(controllerInstance);
+
+        var stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(primaryStage);
+
+        Scene scene = new Scene(loader.load(),width,height);
+        stage.setResizable(resizable);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
+
+        activeStages.put(PurchaseDialogController.class,stage);
+
+        return controllerInstance;
+    }
+
     public <T extends WindowController> void showScene(Class<T> controllerClass) throws Exception {
         var controllerInstance = loadController(controllerClass);
 
@@ -87,6 +128,7 @@ public class DefaultSceneManager implements SceneManager {
         primaryStage.setResizable(controllerInstance.isResizable());
         primaryStage.show();
     }
+
     @Override
     public Stage getPrimaryStage() {
         return this.primaryStage;
@@ -110,6 +152,21 @@ public class DefaultSceneManager implements SceneManager {
     @Override
     public Controller getMarketComponentController() throws Exception {
         return loadControllerComponent(MarketController.class);
+    }
+
+    @Override
+    public Controller createCurrencyComponent() throws Exception {
+        return loadControllerComponent(CurrencyComponent.class);
+    }
+
+    @Override
+    public Controller createPurchaseWindow() throws Exception {
+        if(activeStages.containsKey(PurchaseDialogController.class)) {
+            activeStages.get(PurchaseDialogController.class).close();
+            activeStages.remove(PurchaseDialogController.class);
+        }
+
+        return createDialog(PurchaseDialogController.class,"Purchase currency",250,135,false);
     }
 
     @Override
