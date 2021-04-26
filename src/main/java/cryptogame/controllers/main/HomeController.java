709ed3c87a9e04fee1f01fd33485ebc221749ec4
KDani-99 <porsekin@gmail.com>
2021-04-26 09:29:50 +0200

    Implement Controller interface

--- src/main/java/cryptogame/controllers/main/HomeController.java
+++ src/main/java/cryptogame/controllers/main/HomeController.java
@@ -1,9 +1,21 @@
-package cryptogame.controller.main;
+package cryptogame.controllers.main;
 
+import cryptogame.controllers.Controller;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.chart.AreaChart;
 
-public class HomeController {
+public class HomeController implements Controller {
 
     @FXML public AreaChart<Double,Double>  areaChart;
+
+    @Override
+    public void initialize() throws Exception {
+
+    }
+    @Override
+    public Node getRoot() {
+        return this.areaChart;
+    }
 }
--- src/main/java/cryptogame/controllers/main/NavbarController.java
+++ src/main/java/cryptogame/controllers/main/NavbarController.java
@@ -1,11 +1,16 @@
-package cryptogame.controller.main;
+package cryptogame.controllers.main;
 
+import cryptogame.controllers.Controller;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class NavbarController {
+public class NavbarController implements Controller {
 
     @FXML public VBox vBox;
 
@@ -16,5 +21,15 @@ public class NavbarController {
 
     @FXML private Label loggedInUsernameLabel;
     @FXML private Label balanceLabel;
+
+    @Override
+    public void initialize() throws Exception {
+
+    }
+
+    @Override
+    public Node getRoot() {
+        return this.vBox;
+    }
 
 }
