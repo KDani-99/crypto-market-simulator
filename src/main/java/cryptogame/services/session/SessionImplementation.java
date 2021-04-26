dcfba0597dac79c601c2cfbb0def4493bc3ea057
KDani-99 <porsekin@gmail.com>
2021-04-26 09:21:03 +0200

    Add Session implementation

---
+++ src/main/java/cryptogame/services/session/SessionImplementation.java
@@ -0,0 +1,15 @@
+package cryptogame.services.session;
+
+public class SessionImplementation implements Session {
+
+    private final long userId;
+
+    public SessionImplementation(long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public long getActiveUserId() {
+        return this.userId;
+    }
+}
