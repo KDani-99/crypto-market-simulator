2011848769e2232f30d1219911e34388ea7a4246
KDani-99 <porsekin@gmail.com>
2021-04-26 09:20:42 +0200

    Add Service handler implementation

---
+++ src/main/java/cryptogame/services/ServiceImpl.java
@@ -0,0 +1,65 @@
+package cryptogame.services;
+
+import cryptogame.dao.user.UserDao;
+import cryptogame.services.manager.market.MarketManager;
+import cryptogame.services.manager.scene.SceneManager;
+import cryptogame.services.session.Session;
+import cryptogame.services.session.SessionImplementation;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("serviceHandler")
+public class ServiceImpl implements Service {
+
+    private SceneManager sceneManager;
+    private MarketManager marketManager;
+    private UserDao userDao;
+
+    private Stage primaryStage;
+
+    private Session activeSession;
+
+    public ServiceImpl() {
+
+    }
+
+    @Autowired
+    public ServiceImpl(SceneManager sceneManager,MarketManager marketManager,UserDao userDao) {
+        this.sceneManager = sceneManager;
+        this.marketManager = marketManager;
+        this.userDao = userDao;
+
+        this.activeSession = null;
+    }
+    @Override
+    public SceneManager getSceneManager() {
+        return sceneManager;
+    }
+
+    @Override
+    public MarketManager getMarketManager() {
+        return marketManager;
+    }
+
+    @Override
+    public UserDao getUserDao() {
+        return this.userDao;
+    }
+
+    @Override
+    public Session getSession() {
+        return this.activeSession;
+    }
+
+    @Override
+    public void createSession(long userId) {
+        this.destroyActiveSession();
+        this.activeSession = new SessionImplementation(userId);
+    }
+
+    @Override
+    public void destroyActiveSession() {
+        this.activeSession = null;
+    }
+}
