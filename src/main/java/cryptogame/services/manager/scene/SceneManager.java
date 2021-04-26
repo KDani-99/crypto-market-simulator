cde193c6dbefaf77a100b71e2559db1ee0f65fa2
KDani-99 <porsekin@gmail.com>
2021-04-26 09:22:28 +0200

    Add helper methods to show other scenes (avoid concrete impl)

--- src/main/java/cryptogame/services/manager/scene/SceneManager.java
+++ src/main/java/cryptogame/services/manager/scene/SceneManager.java
@@ -1,10 +1,16 @@
-package cryptogame.service.manager.scene;
+package cryptogame.services.manager.scene;
 
 import cryptogame.common.Initializable;
 import cryptogame.controllers.Controller;
+import cryptogame.controllers.WindowController;
 import javafx.stage.Stage;
 
 public interface SceneManager extends Initializable {
     <T extends WindowController> void showScene(Class<T> controllerClass) throws Exception; // deprecated
     Stage getPrimaryStage();
+    void showMainScene() throws Exception;
+    void showLoginScene() throws Exception;
+    void showRegistrationScene() throws Exception;
+
+    Controller getMarketComponentController() throws Exception;
 }
