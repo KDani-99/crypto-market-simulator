7a565593ae7d3d8671cbaa86f6831538d170bf53
KDani-99 <porsekin@gmail.com>
2021-04-26 09:30:42 +0200

    Controllers should be initializable

--- src/main/java/cryptogame/controllers/Controller.java
+++ src/main/java/cryptogame/controllers/Controller.java
@@ -1,16 +1,12 @@
-package cryptogame.controller;
+package cryptogame.controllers;
 
+import cryptogame.common.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import java.net.URL;
 
-public interface Controller {
-    void initScene();
-    void showError(String message,String alertMessage);
-    void hideError();
-    void createScene(Parent view);
-    Scene getScene();
-    boolean isResizable();
-    URL getResourceURL();
+public interface Controller extends Initializable {
+    Node getRoot();
 }
