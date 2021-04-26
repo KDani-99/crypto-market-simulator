a0c9bebcae1dcb59d2ec1f6710be8b6729d46718
KDani-99 <porsekin@gmail.com>
2021-04-26 09:28:09 +0200

    Add WindowController interface (for main windows)

---
+++ src/main/java/cryptogame/controllers/WindowController.java
@@ -0,0 +1,13 @@
+package cryptogame.controllers;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public interface WindowController extends Controller {
+    void initScene();
+    void showError(String message,String alertMessage);
+    void hideError();
+    void createScene(Parent view);
+    Scene getScene();
+    boolean isResizable();
+}
