a5f9835a82955bdcf13ee2dfa10fde5baf2bb96f
KDani-99 <porsekin@gmail.com>
2021-04-27 14:41:39 +0200

    Add title string

--- src/main/java/cryptogame/services/manager/scene/SceneManager.java
+++ src/main/java/cryptogame/services/manager/scene/SceneManager.java
@@ -6,7 +6,7 @@ import cryptogame.controllers.WindowController;
 import javafx.stage.Stage;
 
 public interface SceneManager extends Initializable {
-    <T extends WindowController> void showScene(Class<T> controllerClass) throws Exception; // deprecated
+    <T extends WindowController> void showScene(Class<T> controllerClass,String title) throws Exception; // deprecated
     Stage getPrimaryStage();
     void showMainScene() throws Exception;
     void showLoginScene() throws Exception;
