18df2465f6380726d504d4ca48719e325ba13b8d
KDani-99 <porsekin@gmail.com>
2021-04-28 18:18:27 +0200

    Remove unnecessary interface methods

--- src/main/java/cryptogame/controllers/WindowController.java
+++ src/main/java/cryptogame/controllers/WindowController.java
@@ -5,8 +5,6 @@ import javafx.scene.Scene;
 
 public interface WindowController extends Controller {
     void initScene();
-    void showError(String message,String alertMessage);
-    void hideError();
     void createScene(Parent view);
     Scene getScene();
 }
