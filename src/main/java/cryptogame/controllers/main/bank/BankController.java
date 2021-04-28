d955caa172d5a7d151342aea9bdbb03d26d147db
KDani-99 <porsekin@gmail.com>
2021-04-28 18:16:21 +0200

    Add BankController

---
+++ src/main/java/cryptogame/controllers/main/bank/BankController.java
@@ -0,0 +1,24 @@
+package cryptogame.controllers.main.bank;
+
+import cryptogame.controllers.Controller;
+import javafx.scene.Node;
+
+public class BankController implements Controller {
+
+
+
+    @Override
+    public Node getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean isResizable() {
+        return false;
+    }
+
+    @Override
+    public void initialize() throws Exception {
+
+    }
+}
