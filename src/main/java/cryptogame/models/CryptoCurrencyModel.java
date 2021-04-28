b8480c42d37f09113f5ba9b7d94f09467f1f59fb
KDani-99 <porsekin@gmail.com>
2021-04-28 18:15:45 +0200

    Remove JoinColumn and add equals & hashCode

--- src/main/java/cryptogame/models/CryptoCurrencyModel.java
+++ src/main/java/cryptogame/models/CryptoCurrencyModel.java
@@ -1,9 +1,6 @@
 package cryptogame.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
@@ -11,12 +8,13 @@ import java.util.Objects;
 public class CryptoCurrencyModel {
 
     @Id
+    @GeneratedValue
+    @Column
     private long id;
     private String idName;
     private double amount;
 
     @ManyToOne
-    @JoinColumn(insertable = false,updatable = false,nullable = false)
     private UserModel user;
 
     @Override
