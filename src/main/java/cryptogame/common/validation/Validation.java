da0ecc09674fa64f184f77a854e4a2ed345cd710
KDani-99 <porsekin@gmail.com>
2021-05-02 11:24:36 +0200

    Split validation code

--- src/main/java/cryptogame/common/validation/Validation.java
+++ src/main/java/cryptogame/common/validation/Validation.java
@@ -1,8 +1,26 @@
 package cryptogame.common.validation;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 
 public abstract class Validation {
+
+    private static <T> HashSet<ValidationError> validateField(Field field, T object, Validate validationAnnotation) throws Exception {
+
+        var result = new HashSet<ValidationError>();
+
+        var validatorInstance = (IValidation<Object>)validationAnnotation.validatorClass().getDeclaredConstructor().newInstance();
+        var value = field.get(object);
+
+        if(!validatorInstance.validate(value)) {
+            result.add(new ValidationError(
+                    field.getName(),
+                    validatorInstance.getErrorMessage()
+            ));
+        }
+
+        return result;
+    }
 
     public static <T> HashSet<ValidationError> validateObject(T object) throws Exception {
 
@@ -13,20 +31,13 @@ public abstract class Validation {
         for(var field : fields) {
 
             var validationAnnotation = field.getAnnotation(Validate.class);
-
             if(validationAnnotation == null) continue;
 
             field.setAccessible(true);
 
-            var validatorInstance = (IValidation<Object>)validationAnnotation.validatorClass().getDeclaredConstructor().newInstance();
-            var value = field.get(object);
+            var fieldResult = validateField(field,object,validationAnnotation);
+            result.addAll(fieldResult);
 
-            if(!validatorInstance.validate(value)) {
-                result.add(new ValidationError(
-                        field.getName(),
-                        validatorInstance.getErrorMessage()
-                ));
-            }
         }
 
         return result;
