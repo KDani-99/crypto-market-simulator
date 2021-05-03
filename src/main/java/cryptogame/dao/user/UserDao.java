69c8794498340e5770c67995ddfa88d0a3b6928f
KDani-99 <porsekin@gmail.com>
2021-05-03 07:32:22 +0200

    Add UserDao specific queries

--- src/main/java/cryptogame/dao/user/UserDao.java
+++ src/main/java/cryptogame/dao/user/UserDao.java
@@ -5,7 +5,11 @@ import cryptogame.dao.Dao;
 import cryptogame.models.UserModel;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 public interface UserDao extends Dao<UserModel>
 {
     void purchaseCurrency(UserModel user, double amount , CurrencyContainer currency);
+    Optional<UserModel> getByUsername(String username);
+    Optional<UserModel> getByEmail(String email);
 }
