7524cbe37a61a45ab092550acdb8a99c539746fd
KDani-99 <porsekin@gmail.com>
2021-05-04 20:57:53 +0200

    Refactor: clean up code

--- src/main/java/cryptogame/services/manager/scene/SceneManagerImplementation.java
+++ src/main/java/cryptogame/services/manager/scene/SceneManagerImplementation.java
@@ -1,10 +1,12 @@
 package cryptogame.services.manager.scene;
 
 import cryptogame.Main;
+import cryptogame.common.Refreshable;
 import cryptogame.controllers.*;
 import cryptogame.controllers.dialog.PurchaseDialogController;
 import cryptogame.controllers.login.LoginController;
-import cryptogame.controllers.main.NavbarController;
+import cryptogame.controllers.main.MainController;
+import cryptogame.controllers.main.navbar.NavbarController;
 import cryptogame.controllers.main.bank.BankController;
 import cryptogame.controllers.main.market.MarketController;
 import cryptogame.controllers.main.market.components.CurrencyComponent;
@@ -23,23 +25,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Optional;
 
 @Component("sceneManager")
 public class SceneManagerImplementation implements SceneManager {
+
+    @lombok.Data
+    static class ControllerInfo {
+        private final URL url;
+        private boolean loaded;
+
+        public ControllerInfo(URL url) {
+            this.url = url;
+        }
+    }
 
     private static final Logger logger = LogManager.getLogger(SceneManagerImplementation.class);
 
     private final ApplicationContext context;
 
-    private final HashMap<Class<?>, URL> controllerCollection;
-    private final HashMap<Class<?>, URL> mainControllerComponents;
+    private final HashMap<Class<?>, ControllerInfo> controllerCollection;
+    private final HashMap<Class<?>, ControllerInfo> mainControllerComponents;
+    private final HashMap<Class<?>, URL> prototypeComponentControllers;
     private final HashMap<Class<?>, URL> dialogControllerCollection;
 
     private final HashMap<Class<?>,Stage> activeStages;
-
-    private final HashMap<Class<?>, Controller> controllerInstances;
 
     private final Stage primaryStage;
 
@@ -50,12 +63,13 @@ public class SceneManagerImplementation implements SceneManager {
         this.controllerCollection = new HashMap<>();
         this.mainControllerComponents = new HashMap<>();
         this.dialogControllerCollection = new HashMap<>();
-        controllerInstances = new HashMap<>();
+        this.prototypeComponentControllers = new HashMap<>();
 
         activeStages = new HashMap<>();
 
         this.addControllers();
         this.addMainControllerComponents();
+        this.addPrototypeComponentControllers();
         this.addDialogControllers();
     }
 
@@ -72,34 +86,49 @@ public class SceneManagerImplementation implements SceneManager {
     }
 
     private void addControllers() {
-        this.controllerCollection.put(MainController.class,getResourceURL("/views/app/AppView.fxml"));
-        this.controllerCollection.put(LoginController.class, getResourceURL("/views/login/LoginView.fxml"));
-        this.controllerCollection.put(RegistrationController.class,getResourceURL("/views/registration/RegistrationView.fxml"));
+        // The scene itself
+        this.controllerCollection.put(MainController.class,new ControllerInfo(getResourceURL("/views/app/AppView.fxml")));
+        this.controllerCollection.put(LoginController.class, new ControllerInfo(getResourceURL("/views/login/LoginView.fxml")));
+        this.controllerCollection.put(RegistrationController.class,new ControllerInfo(getResourceURL("/views/registration/RegistrationView.fxml")));
     }
     private void addMainControllerComponents() {
-        this.mainControllerComponents.put(NavbarController.class,getResourceURL("/views/app/components/navbar/Navbar.fxml"));
-        this.mainControllerComponents.put(StatsComponent.class,getResourceURL("/views/app/components/stats/components/StatsComponent.fxml"));
-        this.mainControllerComponents.put(CurrencyComponent.class,getResourceURL("/views/app/components/market/components/CurrencyComponent.fxml"));
-
-        this.mainControllerComponents.put(MarketController.class,getResourceURL("/views/app/components/market/MarketView.fxml"));
-        this.mainControllerComponents.put(BankController.class,getResourceURL("/views/app/components/bank/BankView.fxml"));
-        this.mainControllerComponents.put(StatsController.class,getResourceURL("/views/app/components/stats/StatsView.fxml"));
+        // Loaded into a scene
+        this.mainControllerComponents.put(NavbarController.class,new ControllerInfo(getResourceURL("/views/app/components/navbar/Navbar.fxml")));
+        this.mainControllerComponents.put(MarketController.class,new ControllerInfo(getResourceURL("/views/app/components/market/MarketView.fxml")));
+        this.mainControllerComponents.put(BankController.class,new ControllerInfo(getResourceURL("/views/app/components/bank/BankView.fxml")));
+        this.mainControllerComponents.put(StatsController.class,new ControllerInfo(getResourceURL("/views/app/components/stats/StatsView.fxml")));
     }
+
+    private void addPrototypeComponentControllers() {
+        // Reusable component
+        this.prototypeComponentControllers.put(CurrencyComponent.class, getResourceURL("/views/app/components/market/components/CurrencyComponent.fxml"));
+        this.prototypeComponentControllers.put(StatsComponent.class,getResourceURL("/views/app/components/stats/components/StatsComponent.fxml"));
+    }
+
     private void addDialogControllers() {
+        // Dialog windows
         this.dialogControllerCollection.put(PurchaseDialogController.class,getResourceURL("/views/dialog/purchase/PurchaseDialogView.fxml"));
+    }
+
+    private <T> T loadFXML(URL url, Controller controllerInstance) throws IOException {
+        var loader = new FXMLLoader(url);
+        loader.setController(controllerInstance);
+
+        return loader.load();
     }
 
     private <T extends WindowController> WindowController loadController(Class<T> controller) throws Exception {
 
         var controllerInstance = context.getBean(controller);
 
-        var resourceUrl = controllerCollection.get(controller);
-        var loader = new FXMLLoader(resourceUrl);
-        loader.setController(controllerInstance);
+        var controllerInfo = controllerCollection.get(controller);
 
-        var view = (Parent)loader.load();
-        controllerInstance.createScene(view);
-        controllerInstance.initScene();
+        if(!controllerInfo.isLoaded()) {
+            var view = (Parent) loadFXML(controllerInfo.url, controllerInstance);
+            controllerInstance.createScene(view);
+            controllerInstance.initScene();
+            controllerInfo.setLoaded(true);
+        }
 
         return controllerInstance;
     }
@@ -107,15 +136,27 @@ public class SceneManagerImplementation implements SceneManager {
     private <T extends Controller> Controller loadControllerComponent(Class<T> controller) throws Exception {
         var controllerInstance = context.getBean(controller);
 
-        var resourceUrl = mainControllerComponents.get(controller);
+        var controllerInfo = mainControllerComponents.get(controller);
 
-        var loader = new FXMLLoader(resourceUrl);
-        loader.setController(controllerInstance);
-
-        loader.load();
+        if(!controllerInfo.isLoaded()) {
+            loadFXML(controllerInfo.url, controllerInstance);
+            controllerInfo.setLoaded(true);
+        }
 
         return controllerInstance;
     }
+
+    private <T extends Controller> Controller createPrototypeComponentController(Class<T> controller) throws Exception {
+
+        var controllerInstance = context.getBean(controller);
+
+        var resourceUrl = this.prototypeComponentControllers.get(controller);
+
+        loadFXML(resourceUrl, controllerInstance);
+
+        return controllerInstance;
+    }
+
 
     private <T extends Controller> Controller createDialog(Class<T> dialog,String title,int width, int height, boolean resizable) throws Exception {
         var controllerInstance = (Controller) context.getBean(dialog);
@@ -143,10 +184,6 @@ public class SceneManagerImplementation implements SceneManager {
     public <T extends WindowController> WindowController showScene(Class<T> controllerClass,String title) throws Exception {
         var controllerInstance = loadController(controllerClass);
 
-        if(!controllerInstances.containsKey(controllerInstance.getClass())) {
-            controllerInstances.put(controllerInstance.getClass(),controllerInstance);
-        }
-
         primaryStage.setScene(controllerInstance.getScene());
         primaryStage.setResizable(controllerInstance.isResizable());
         primaryStage.setTitle(title);
@@ -162,6 +199,8 @@ public class SceneManagerImplementation implements SceneManager {
 
     @Override
     public WindowController showMainScene() throws Exception {
+        primaryStage.setWidth(850);
+        primaryStage.setHeight(500);
         return showScene(MainController.class,"Main");
     }
 
@@ -174,6 +213,8 @@ public class SceneManagerImplementation implements SceneManager {
 
     @Override
     public WindowController showRegistrationScene() throws Exception {
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(500);
         return showScene(RegistrationController.class,"Registration");
     }
 
@@ -204,12 +245,12 @@ public class SceneManagerImplementation implements SceneManager {
 
     @Override
     public Controller createStatsComponent() throws Exception {
-        return loadControllerComponent(StatsComponent.class);
+        return createPrototypeComponentController(StatsComponent.class);
     }
 
     @Override
     public Controller createCurrencyComponent() throws Exception {
-        return loadControllerComponent(CurrencyComponent.class);
+        return createPrototypeComponentController(CurrencyComponent.class);
     }
 
     @Override
@@ -234,5 +275,43 @@ public class SceneManagerImplementation implements SceneManager {
     @Override
     public void initialize() {
         this.loadDefaultScene();
+    }
+
+
+    private <T> Optional<ControllerInfo> findControllerInfoOfType(Class<T> controllerType) {
+        // Finds the refreshable components of a given class
+
+        for(var controller : controllerCollection.keySet()) {
+            if(controller.isAssignableFrom(controllerType))
+                return Optional.of(controllerCollection.get(controller));
+        }
+
+        for(var controller : mainControllerComponents.keySet()) {
+            if(controller.isAssignableFrom(controllerType))
+                return Optional.of(mainControllerComponents.get(controller));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void refresh() {
+        try {
+            for(var controller : context.getBeansOfType(Refreshable.class).values()) {
+                var controllerInfo = findControllerInfoOfType(controller.getClass());
+                if(controllerInfo.isPresent() && controllerInfo.get().isLoaded()) {
+                    controller.refresh();
+                }
+            }
+        } catch (Exception exception) {
+            logger.error(exception);
+        }
+    }
+    @Override
+    public void onExit() {
+        for(var controller : context.getBeansOfType(Controller.class).values()) {
+            controller.onExit();
+            logger.info("Called onExit event on " + controller.getClass().getName() + "controller instance");
+        }
     }
 }
