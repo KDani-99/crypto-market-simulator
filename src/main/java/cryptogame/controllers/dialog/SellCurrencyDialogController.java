4e5d0b3b7471612b66b0863b9b1a3729affc5119
KDani-99 <porsekin@gmail.com>
2021-05-05 22:54:07 +0200

    Add SellCurrencyDialogController

---
+++ src/main/java/cryptogame/controllers/dialog/SellCurrencyDialogController.java
@@ -0,0 +1,12 @@
+package cryptogame.controllers.dialog;
+
+import cryptogame.controllers.Controller;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class SellCurrencyDialogController implements Controller {
+    
+}
