333aa2ada6be2a0e7d0b1aa3a7cf6a41ad0d0003
KDani-99 <porsekin@gmail.com>
2021-05-06 21:16:03 +0200

    Remove unused interface methods

--- src/main/java/cryptogame/model/dao/Dao.java
+++ src/main/java/cryptogame/model/dao/Dao.java
@@ -3,10 +3,6 @@ package cryptogame.model.dao;
 import java.util.Optional;
 
 public interface Dao<T> {
-
     <TId> Optional<T> getEntity(TId id);
-    void updateEntity(T entity);
     void persistEntity(T entity);
-    void deleteEntity(T entity);
-
 }
