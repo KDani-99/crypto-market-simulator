fd04ad24086b6c0422ce69190cddbbf26a78256e
KDani-99 <porsekin@gmail.com>
2021-05-06 21:14:11 +0200

    Add transactionManager Bean

--- src/main/java/cryptogame/AppConfig.java
+++ src/main/java/cryptogame/AppConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 @Configuration
 @ComponentScan
+@EnableTransactionManagement
 public class AppConfig {
 
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("crypto_trading_game");
@@ -18,6 +23,15 @@ public class AppConfig {
     @Bean
     public EntityManagerFactory entityManagerFactory() {
         return entityManagerFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory );
+        transactionManager.setNestedTransactionAllowed(true);
+
+        return transactionManager;
     }
 
     @Primary
