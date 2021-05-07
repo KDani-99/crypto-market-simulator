df0acd6855f8dd0e3e744d61e0e881182287b47a
KDani-99 <porsekin@gmail.com>
2021-05-07 15:57:59 +0200

    Add reset interface method

--- src/main/java/cryptogame/controllers/Controller.java
+++ src/main/java/cryptogame/controllers/Controller.java
@@ -1,11 +1,14 @@
 package cryptogame.controllers;
 
-import cryptogame.common.Initializable;
+import cryptogame.common.interfaces.Initializable;
 import javafx.scene.Node;
 
 public interface Controller extends Initializable {
     Node getRoot();
     boolean isResizable();
+    default void reset() {
+        // empty
+    }
     default void onExit() {
         // empty
     }
